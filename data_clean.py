6978d83f6a467cf00eb15eb89b688b49a3bce81a
pickou <472179216@qq.com>
2016-10-20 23:38:25 +0800

    Update data_clean.py

--- data_clean.py
+++ data_clean.py
@@ -1,26 +1,15 @@
-﻿import pandas as pd
+import pandas as pd
 from pandas import DataFrame
-test_name_index=["rank","dt","cookie","ip","idfa","imei","android","openudid",\
-                  "mac","timestamps","camp","creativeid","mobile_os","mobile_type",\
-                  "app_key_md5","app_name_md5","placementid","useragent","mediaid",\
-                  "os_type","born_time"]
-train_name_index=test_name_index+["label"]
-test_name_index=["rank","dt","cookie","ip","idfa","imei","android","openudid",\
-                  "mac","timestamps","camp","creativeid","mobile_os","mobile_type",\
-                  "app_key_md5","app_name_md5","placementid","useragent","mediaid",\
-                  "os_type","born_time"]
-test_type={'rank':int,'dt':int,'cookie':object,'ip':object,'idfa':object,'imei':object,\
-            'android':object,'openudid':object,'mac':object,'timestamps':int,'camp':int,\
-            'creativeid':int,'mobile_os':object,'mobile_type':object,'app_key_md5':object,\
-            'app_name_md5':object,'placementid':object,'useragent':object,'mediaid':int,\
-            'os_type':object,'born_time':int}
-train_type=dict({'label':int},**test_type)
+name_index=["rank","dt","cookie","ip","label"]
+col_type={'rank':int,'dt':int,'cookie':object,'ip':object,'label':int}
+name=['AdMaster_train_dataset','test_data']
+choose=0#choose train(0) or test(1)
+file_name=['train_data_csv','test_data_csv']
 #从7天的日志中统计每天的数据
 for i in range(1,8):
-    pd_file=pd.read_csv('AdMaster_train_dataset_1000000_{0}.csv'.format(i),names=train_name_index,dtype=train_type )
-    data_needed=pd_file[['rank','ip','cookie','label']]
+    data_needed=pd.read_csv('./{0}/{1}_{2}.csv'.format(file_name[choose],name[choose],i),names=name_index,dtype=col_type)
     #ip在一天内出现的次数
-    ip_temp=pd_file[['ip','label']].groupby(['ip'])
+    ip_temp=data_needed[['ip','label']].groupby(['ip'])
     ip_count=ip_temp.count()
     #同一个ip作弊的次数
     ip_cheat=ip_temp.sum()
@@ -34,7 +23,7 @@ for i in range(1,8):
     #从内存中把不需要的数据删除
     del ip_temp,ip_count
     #cookie在一天内出现的次数
-    cookie_temp=pd_file[['cookie','label']].groupby(['cookie'])
+    cookie_temp=data_needed[['cookie','label']].groupby(['cookie'])
     cookie_count=cookie_temp.count()
     #同一个cookie作弊的次数
     cookie_cheat=cookie_temp.sum()
@@ -45,7 +34,7 @@ for i in range(1,8):
     #从内存中把不需要的数据删除
     del cookie_temp,cookie_count
     #ip_cookie出现的次数
-    ip_cookie_temp=pd_file[['ip','cookie','label']].groupby(['ip','cookie'])
+    ip_cookie_temp=data_needed[['ip','cookie','label']].groupby(['ip','cookie'])
     ip_cookie_count=ip_cookie_temp.count()
     ip_cookie_cheat=ip_cookie_temp.sum()
     #将grouby之后的index还原为column
@@ -56,5 +45,7 @@ for i in range(1,8):
     data_needed=data_needed.sort_values(by='rank')
     #if you don‘t need ip just drop it
     data_needed.drop(['ip','cookie'],axis=1,inplace=True)
-    data_needed.to_csv('data_to_use{0}.csv'.format(i))
+    data_needed.to_csv('./{0}/{0}_to_use_{1}.csv'.format(file_name[choose],i),index=False)
+    del data_needed
+    print i
 
